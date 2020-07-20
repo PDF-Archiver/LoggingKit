1ad81013e4a24cfe1c7541adef09c581aa61c763
Julian Kahnert <Julian.Kahnert@worldiety.de>
2020-07-20 22:36:53 +0200

    fix macos version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "LoggingKit",
     platforms: [
         .iOS(.v11),
-        .macOS(.v10_12)
+        .macOS(.v10_13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
